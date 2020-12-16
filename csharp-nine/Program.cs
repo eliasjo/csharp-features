595027f9c645998a87a9dba581c4ae50f68880aa
Jonas Elias <jonas.elias@trumpf.com>
2020-12-16 18:18:30 +0100

    first try with record types c# 9

---
+++ csharp-nine/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace csharp_nine
+{
+    public record Type(string Name, int Value) : Extender(false);
+
+    public record Extender(bool FromCache);
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var type = new Type("Hugo", 123);
+            var item = type.LoadFromCache();
+        }
+    }
+
+    public static class GenericExtenderExtension
+    {
+        public static T LoadFromCache<T>(this T item) where T : Extender
+        {
+            return item with { FromCache = true};
+        }
+    }
+}
